c0bf2bf4f071912cd2a8706a596b0830faa39b16
Owen Wang <wangfowen@gmail.com>
2017-07-27 14:19:40 -0700

    converted progress to posts

--- _posts/2017-07-23-welcome-to-jekyll.markdown
+++
@@ -1,7 +0,0 @@
----
-layout: post
-title:  "Test"
-date:   2017-07-23 16:39:54 -0700
----
-
-Test
---
+++ js/progress.js
@@ -0,0 +1,14 @@
+(function($) {
+  skel.breakpoints({
+    xlarge:   '(max-width: 1280px)',
+    large:    '(max-width: 1280px)',
+    medium:   '(max-width: 980px)',
+    small:    '(max-width: 736px)',
+    xsmall:   '(max-width: 480px)',
+    xxsmall:  '(max-width: 360px)'
+  });
+
+  skel.layout({
+    grid: true
+  });
+});
