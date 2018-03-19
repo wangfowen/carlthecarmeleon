b731687b1f5728b1558d21cab95b29750af52e85
Owen Wang <wangfowen@gmail.com>
2018-03-18 18:12:55 -0700

    basic designs

--- js/main.js
+++ js/main.js
@@ -1,23 +1,37 @@
 $(function() {
   var $modal = $('#modal');
   var $content = $('#content');
+  var $menu = $('#mobile-menu');
 
   function link(el, link) {
     el.click(function(e) {
       e.preventDefault();
+      e.stopPropagation();
       $content.load(link);
       $modal.show();
     });
   }
 
-  link($('#eye'), "/blurbs/eye.html");
-  link($('#fire'), "/blurbs/fire.html");
-  link($('#led'), "/blurbs/led.html");
-  link($('#music'), "/blurbs/music.html");
+  link($('.eye'), "/blurbs/eye.html");
+  link($('.fire'), "/blurbs/fire.html");
+  link($('.led'), "/blurbs/led.html");
+  link($('.music'), "/blurbs/music.html");
+  link($('.car'), "/blurbs/car.html");
+  link($('.tail'), "/blurbs/tail.html");
 
   $('#close').click(function(e) {
     e.preventDefault();
+    e.stopPropagation();
     $modal.hide();
+  });
+
+  $('#bg').click(function(e) {
+    e.preventDefault();
+    $modal.hide();
+  });
+
+  $('#menu-open').click(function(e) {
+    $menu.toggle();
   });
 });
 
